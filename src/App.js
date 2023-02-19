6d62e52eec31d86ffc8df4deb26eeee77fb83be1
YAYA <yahyaabulhaj@outlook.com>
2023-02-19 18:29:31 +0100

    initial Project Stats: Building in Public from0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useSpring, animated } from 'react-spring';
+import Navigation from './Navigation';
 
 function App() {
+  const headerProps = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: 1000 },
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <animated.header className="App-header" style={headerProps}>
+        <h1>Welcome to My Accomplishments!</h1>
+      </animated.header>
+      <main>
+        <p>This is the home page of my website. Here, I'll showcase my accomplishments and provide resources for others to achieve their goals.</p>
+      </main>
     </div>
   );
 }
---
+++ src/Navigation.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Navigation.css';
+
+function Navigation() {
+  return (
+    <div className="Navigation">
+        <img src="public/logo192.png" alt="My Logo" />    
+      <ul>
+        <li><a href="#">FAQ</a></li>
+        <li><a href="#">Blogs</a></li>
+        <li><a href="#">Resources</a></li>
+        <li><a href="#">Notes</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navigation;
